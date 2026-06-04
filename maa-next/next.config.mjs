b2f1e7ea26146b4e4e9211e00fdeb8d05a846fb8
ChargingTrex <arunbalajisathya@gmail.com>
2026-06-04 16:39:45 +0530

    chore: stop tracking old maa-next and maa-website directories

--- maa-next/next.config.mjs
+++
@@ -1,8 +0,0 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.js');
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default withNextIntl(nextConfig);
